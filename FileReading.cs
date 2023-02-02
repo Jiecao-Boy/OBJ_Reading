29911a2316fe442a2a4816ceefebb7044f34857a
JiecaoBoy <wxdyl0915@gmail.com>
2023-02-02 00:38:22 -0500

    This is a half-way attempt of reading an obj file and generate a mesh

--- FileReading.cs
+++ FileReading.cs
@@ -8,10 +8,28 @@ using System.Linq;
 public class FileReading : MonoBehaviour
 {
     public Transform contentWindow;
-
     public GameObject recallTextObject; 
+
+	public int xSize, ySize, zSize;
+	private Mesh mesh;
+	private Vector3[] vertices;
+
+    private void Awake () {
+		Generate();
+	}
+
+	private void Generate () {
+		GetComponent<MeshFilter>().mesh = mesh = new Mesh();
+		mesh.name = "Procedural Cube";
+		CreateMesh ();
+    }
+
     // Start is called before the first frame update
-    void Start()
+	private void setVertex (int i, int x, int y, int z) {
+		vertices[i] = new Vector3(x, y, z);
+	}
+
+    private void CreateMesh()
     {
         string readFromFilePath = Application.streamingAssetsPath +"/"+"teapot"+".obj";
 
@@ -19,11 +37,45 @@ public class FileReading : MonoBehaviour
 
         Instantiate(recallTextObject, contentWindow);
 
+       //foreach (string line in fileLines)
+        //{s
+        //    recallTextObject.GetComponent<Text>().text += "\n" + line; 
+        //} 
+
+
+        
+        //Try to parse the list
+        char[] separator = {' '}; 
+        int v = 0; 
+
         foreach (string line in fileLines)
         {
-            recallTextObject.GetComponent<Text>().text += "\n" + line; 
-        } 
+            //First split the string
+            string[] splitArray = line.Split(separator);
+            string head = splitArray[0];
+            if (head == "v" && line.Length > 1){
+                //Vector3 vertex = new Vector3(0,0,0); 
+                setVertex(v++,0,0,0);
+            }
+            //if (head == "f"){
+
+            //}
+            mesh.vertices = vertices; 
+            Debug.Log(vertices);
+            recallTextObject.GetComponent<Text>().text += "\n"+ head;
+            //foreach(char c in line){
+                //new string:
+                //StringBuilder sb = new StringBuilder();
+                //append:
+                //sb.append(c);
+                //char.IsLetter(c) && sb.Length == 0;
+                //char.IsNumber(c)
+
+
+        }
     }
 
-
 }
+
+
+
