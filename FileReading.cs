edb86539ba0623d92245cb776b3b634977a1618b
JiecaoBoy <wxdyl0915@gmail.com>
2023-02-02 14:56:16 -0500

    obj to mesh

--- FileReading.cs
+++ FileReading.cs
@@ -5,11 +5,9 @@ using UnityEngine.UI;
 using System.IO;
 using System.Linq;
 
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
 public class FileReading : MonoBehaviour
 {
-    public Transform contentWindow;
-    public GameObject recallTextObject; 
-
 	public int xSize, ySize, zSize;
 	private Mesh mesh;
 	private Vector3[] vertices;
@@ -25,55 +23,89 @@ public class FileReading : MonoBehaviour
     }
 
     // Start is called before the first frame update
-	private void setVertex (int i, int x, int y, int z) {
+	private void setVertex (int i, float x, float y, float z) {
 		vertices[i] = new Vector3(x, y, z);
 	}
 
     private void CreateMesh()
     {
-        string readFromFilePath = Application.streamingAssetsPath +"/"+"teapot"+".obj";
-
+        string readFromFilePath = Application.streamingAssetsPath +"/"+"teddy"+".obj";//replace with any obj file
         List<string> fileLines = File.ReadAllLines(readFromFilePath).ToList();
 
-        Instantiate(recallTextObject, contentWindow);
 
-       //foreach (string line in fileLines)
-        //{s
-        //    recallTextObject.GetComponent<Text>().text += "\n" + line; 
-        //} 
-
-
-        
-        //Try to parse the list
+        //Parse the list
         char[] separator = {' '}; 
         int v = 0; 
-
+        int f = 0; 
+        //Determine the length of vertices and faces/triangles
         foreach (string line in fileLines)
         {
-            //First split the string
+            //split the string
             string[] splitArray = line.Split(separator);
             string head = splitArray[0];
             if (head == "v" && line.Length > 1){
-                //Vector3 vertex = new Vector3(0,0,0); 
-                setVertex(v++,0,0,0);
+                v++;
             }
-            //if (head == "f"){
-
-            //}
-            mesh.vertices = vertices; 
-            Debug.Log(vertices);
-            recallTextObject.GetComponent<Text>().text += "\n"+ head;
-            //foreach(char c in line){
-                //new string:
-                //StringBuilder sb = new StringBuilder();
-                //append:
-                //sb.append(c);
-                //char.IsLetter(c) && sb.Length == 0;
-                //char.IsNumber(c)
-
-
+            if (head == "f" && line.Length > 1){
+                f++;
+            }
         }
+
+        //Set up vertices
+        vertices = new Vector3[v];
+        int i = 0;
+        foreach (string line in fileLines)
+        {
+            //split the string
+            string[] splitArray = line.Split(separator);
+            string head = splitArray[0];
+
+            if (head == "v" && line.Length > 1){
+                float x=float.Parse(splitArray[1]);
+                float y=float.Parse(splitArray[2]);
+                float z=float.Parse(splitArray[3]);
+                setVertex(i++,x,y,z);
+            }
+        }
+        mesh.vertices = vertices;
+
+        //Set up faces/triangles
+        //first have to determine the number of triangles\
+        //here we make an assumple that one face only features one triangle
+        Debug.Log(f);
+        int[] triangles = new int[f*3];
+        Debug.Log("so far so good");
+        int j = 0; 
+        foreach (string line in fileLines)
+        {
+            //split the string
+            string[] splitArray = line.Split(separator);
+            string head = splitArray[0];
+            if (head == "f" && line.Length > 1){
+                triangles[j*3] = int.Parse(splitArray[1])-1;
+                triangles[j*3+1] = int.Parse(splitArray[2])-1;
+                Debug.Log(splitArray[2]);
+                Debug.Log(splitArray[3]);
+                triangles[j*3+2] = int.Parse(splitArray[3])-1;
+                j++;
+
+                mesh.triangles = triangles;
+            }
+        }
+        Debug.Log(j);
+
     }
+
+    //draw gizmos to see if vertices are renderred correctly
+	private void OnDrawGizmos () {
+		if (vertices == null) {
+			return;
+		}
+		for (int i = 0; i < vertices.Length; i++) {
+			Gizmos.color = Color.black;
+			Gizmos.DrawSphere(vertices[i], 0.1f);
+		}
+	}
 
 }
 
